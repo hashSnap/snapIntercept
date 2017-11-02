42f759ccc5a3f15ee6dcd31515703eb58c8ef530
hashSnap <andressser@hotmail.com>
2017-11-02 18:21:36 +0100

    Updated for Snapchat 10.20.5.0.

--- app/src/main/java/local/interceptmod/modules/Obfuscator.java
+++ app/src/main/java/local/interceptmod/modules/Obfuscator.java
@@ -2,24 +2,24 @@ package local.interceptmod.modules;
 
 interface Obfuscator {
     // Snapchat version
-    int VersionCode = 1380;
-    String ExpectedVersion = "10.19.5.0";
+    int VersionCode = 1385;
+    String ExpectedVersion = "10.20.5.0";
     // Snap event
-    String SnapEventKlass = "mff"; // "-FirstFrame"
+    String SnapEventKlass = "mlv"; // "-FirstFrame"
     String SnapEventGetCacheKey = "L";
-    String SnapEventIsVideo = "do_";
-    String SnapEventUsername = "an";
-    String SnapEventTimestamp = "t";
-    String SnapEventIsZipped= "ao";
+    String SnapEventIsVideo = "da_";
+    String SnapEventUsername = "am";
+    String SnapEventTimestamp = "v";
+    String SnapEventIsZipped= "an";
     // Media cache
-    String MediaCacheEntryKlass = "mjt"; // ("mCache", this.a).a("mKey", this.b)
-    String MediaCacheEntryConstructorFirstParam = "ole";
+    String MediaCacheEntryKlass = "mql"; // ("mCache", this.a).a("mKey", this.b)
+    String MediaCacheEntryConstructorFirstParam = "otd";
     // Encryption
     String CbcEncryptionAlgorithmKlass = "com.snapchat.android.framework.crypto.CbcEncryptionAlgorithm"; 
     String CbcEncryptionAlgorithmDecrypt = "b"; 
     String EncryptionAlgorithmInterface = "com.snapchat.android.framework.crypto.EncryptionAlgorithm"; 
     // Root detection
-    String RootDetectorKlass = "nps"; // "/sbin/su", "/system/bin/su",
+    String RootDetectorKlass = "nyc"; // "/sbin/su", "/system/bin/su",
     String RootDetectorFirst = "b"; 
     String RootDetectorSecond = "c"; 
     String RootDetectorThird = "d"; 
