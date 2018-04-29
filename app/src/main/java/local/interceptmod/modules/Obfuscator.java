38c1bed7925a09b6cf490ec410bdb72b01d179c8
fulgerul46 <fulgerul46@Yahoo.com>
2018-04-29 20:06:00 +0300

    update to 10.30.5.0

--- app/src/main/java/local/interceptmod/modules/Obfuscator.java
+++ app/src/main/java/local/interceptmod/modules/Obfuscator.java
@@ -2,24 +2,24 @@ package local.interceptmod.modules;
 
 interface Obfuscator {
     // Snapchat version
-    int VersionCode = 1681;
-    String ExpectedVersion = "10.29.5.0";
+    int VersionCode = 1691;
+    String ExpectedVersion = "10.30.5.0";
     // Snap event - everything here is in the file of SnapEventKlass
-    String SnapEventKlass = "tqf"; // find the file with "-FirstFrame" in it
+    String SnapEventKlass = "uuv"; // find the file with "-FirstFrame" in it
     String SnapEventGetCacheKey = "ag"; // return ag() + "-FirstFrame";
-    String SnapEventIsVideo = "i"; // in the (i()) in the if statement above: return uvm.SNAP_VIDEO_RECEIVED ,search for SNAP_VIDEO_RECEIVED;
+    String SnapEventIsVideo = "i"; // in the (i()) in the if statement above: return wab.SNAP_VIDEO_RECEIVED ,search for SNAP_VIDEO_RECEIVED;
     String SnapEventUsername = "aB"; //  this.aB = str2;
     String SnapEventTimestamp = "w"; // this.w = j2;
     String SnapEventIsZipped= "aC"; // "isZipped", this.aC
     // Media cache - everything here is in the file of MediaCacheEntryKlass
-    String MediaCacheEntryKlass = "tuu"; // find the file with ("mCache", this.a).a("mKey", this.b) in it
-    String MediaCacheEntryConstructorFirstParam = "weu"; // public tuu(weu weu, String str) {this(weu, str, null, false);}
+    String MediaCacheEntryKlass = "uzi"; // find the file with ("mCache", this.a).a("mKey", this.b) in it
+    String MediaCacheEntryConstructorFirstParam = "xiy"; // public uzi(xiy xiy, String str) {this(xiy, str, null, false);}
     // Encryption
     String CbcEncryptionAlgorithmKlass = "com.snapchat.android.framework.crypto.CbcEncryptionAlgorithm";
     String CbcEncryptionAlgorithmDecrypt = "b";
     String EncryptionAlgorithmInterface = "com.snapchat.android.framework.crypto.EncryptionAlgorithm";
     // Root detection
-    String RootDetectorKlass = "vhm"; // "/sbin/su", "/system/bin/su"
+    String RootDetectorKlass = "wma"; // "/sbin/su", "/system/bin/su"
     String RootDetectorFirst = "b";
     String RootDetectorSecond = "c";
     String RootDetectorThird = "d";
