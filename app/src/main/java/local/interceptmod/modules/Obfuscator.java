333c57d985217dcb71dd05649a332a80124b2cab
fulgerul46 <fulgerul46@Yahoo.com>
2018-07-11 01:54:09 +0300

    update for 10.35.5.0

--- app/src/main/java/local/interceptmod/modules/Obfuscator.java
+++ app/src/main/java/local/interceptmod/modules/Obfuscator.java
@@ -2,24 +2,24 @@ package local.interceptmod.modules;
 
 interface Obfuscator {
     // Snapchat version
-    int VersionCode = 1708;
-    String ExpectedVersion = "10.31.5.0";
+    int VersionCode = 1741;
+    String ExpectedVersion = "10.35.5.0";
     // Snap event - everything here is in the file of SnapEventKlass
-    String SnapEventKlass = "vqb"; // find the file with "-FirstFrame" in it
-    String SnapEventGetCacheKey = "ai"; // return ai() + "-FirstFrame";
-    String SnapEventIsVideo = "i"; // in the (i()) in the if statement above: return wuf.SNAP_VIDEO_RECEIVED ,search for SNAP_VIDEO_RECEIVED;
-    String SnapEventUsername = "aE"; //  this.aE = str2;
-    String SnapEventTimestamp = "w"; // this.w = j2;
-    String SnapEventIsZipped= "aF"; // "isZipped", this.aF
+    String SnapEventKlass = "abbr"; // find the file with "-FirstFrame" in it
+    String SnapEventGetCacheKey = "k"; // return ai() + "-FirstFrame";
+    String SnapEventIsVideo = "aF_"; // in the (aF_()) in the if statement above: return achn.SNAP_VIDEO_RECEIVED ,search for SNAP_VIDEO_RECEIVED;
+    String SnapEventUsername = "aF"; //  this.aE = str2;
+    String SnapEventTimestamp = "y"; // this.w = j2;
+    String SnapEventIsZipped= "aG"; // "isZipped", this.aG
     // Media cache - everything here is in the file of MediaCacheEntryKlass
-    String MediaCacheEntryKlass = "vuq"; // find the file with ("mCache", this.a).a("mKey", this.b) in it
-    String MediaCacheEntryConstructorFirstParam = "yer"; //  public vuq(yer yer, String str) { this(yer, str, null, false);}
+    String MediaCacheEntryKlass = "abgd"; // find the file with ("mCache", this.a).a("mKey", this.b) in it
+    String MediaCacheEntryConstructorFirstParam = "adtu"; //  public abgd(adtu adtu, String str) { this(adtu, str, null, false);}
     // Encryption
     String CbcEncryptionAlgorithmKlass = "com.snapchat.android.framework.crypto.CbcEncryptionAlgorithm";
     String CbcEncryptionAlgorithmDecrypt = "b";
     String EncryptionAlgorithmInterface = "com.snapchat.android.framework.crypto.EncryptionAlgorithm";
     // Root detection
-    String RootDetectorKlass = "xho"; // "/sbin/su", "/system/bin/su"
+    String RootDetectorKlass = "actu"; // "/sbin/su", "/system/bin/su"
     String RootDetectorFirst = "b";
     String RootDetectorSecond = "c";
     String RootDetectorThird = "d";
